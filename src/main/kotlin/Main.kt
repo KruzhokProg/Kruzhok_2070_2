8447785f25b7904437e38d6da63c80110f234200
Nikolay Irtuganov <vongu3@gmail.com>
2024-09-15 17:30:06 +0300

    операции + Шифр Цезаря + ДЗ

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,69 @@
 package org.example
 
 fun main() {
-    println("Hello World!")
+    // x = 2
+    // y = 3
+//    var x: Int = 2
+//    var y: Int = 3
+////    var z: Int
+////    x,y = y,x
+//    print(x)
+//    print(y)
+
+//    z = x
+//    x = y
+//    y = z
+//    x = x + y // 5
+//    y = x - y // 5 - 3 = 2
+//    x = x - y // 5 - 2 = 3
+
+//    println()
+//    print(x)
+//    print(y)
+    // x = 3
+    // y = 2
+//    ---------------------
+//    +, -, *, /, %
+//    var x = 1234
+//    var a1 = x / 1000
+//    var a2 = (x / 100) % 10
+//    var a3 = (x % 100) / 10
+//    var a4 = x % 10
+//    println(a1)
+//    println(a2)
+//    println(a3)
+//    println(a4)
+//    var res: Int = a4 * 1000 + a3 * 100 + a2 * 10 + a1
+//    println(res)
+//    --------------------
+//    var x = readln().toInt()
+//    var a1 = (x / 81) % 3
+//    var a2 = (x / 27) % 3
+//    var a3 = (x / 9) % 3
+//    var a4 = (x / 3) % 3
+//    var a5 = x % 3
+//
+//    print(a1)
+//    print(a2)
+//    print(a3)
+//    print(a4)
+//    print(a5)
+    // zara
+    //
+    var x1: Char = 'z'
+    var y1: Char = 'a'
+    var z1: Char = 'r'
+    var w1: Char = 'a'
+
+    var shift = -3
+    var x2: Char = ((x1.toInt() - 97 + shift) % 26 + 97).toChar()
+    var y2: Char = ((y1.toInt() - 97 + shift) % 26 + 97).toChar()
+    var z2: Char = ((z1.toInt() - 97 + shift) % 26 + 97).toChar()
+    var w2: Char = ((w1.toInt() - 97 + shift) % 26 + 97).toChar()
+    print(x2)
+    print(y2)
+    print(z2)
+    print(w2)
+    // ДЗ: Дописать программу шифра Цезаря для отрицательных шагов
+    // когда s = -1,-2,... a -> z, y
 }
